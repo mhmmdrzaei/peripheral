3af97a55a3aa93562d56137d5a5faab02ad0e06d
Mohammad <mhmmdrzaei@gmail.com>
2022-07-27 08:03:02 -0400

    mobile site stylings

--- page-home.php
+++ page-home.php
@@ -6,7 +6,7 @@
  
 </main>
 </section>
-<section class="pageMain">
+<section class="pageMain homepageMain">
   <section class="homepageSVG">
     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
        viewBox="0 0 2309.74 2654.88" style="enable-background:new 0 0 2309.74 2654.88;" xml:space="preserve">
